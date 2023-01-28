1f9e95e539ba63370d0192be5f2782ac8783c54f
Beamdamon <Beamdamon@gmail.com>
2023-01-28 10:39:52 -0700

    File Setup

--- BST Project.cpp
+++ BST Project.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <cstdlib>
+
+#include "BST.cpp"
+
 using namespace std;
 
 int main()
 {
     
+    
+    
+    return 0;
 }
---
+++ BST.h
@@ -0,0 +1,20 @@
+class BST 
+{
+
+private:
+	
+	struct node
+	{
+		int key;
+		node* left;
+		node* right;
+	};
+
+	node* root;
+
+public:
+
+	BST();
+	node* CreateLeaf(int key);
+
+};
